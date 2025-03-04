7d8d5c225597dac1894c2521353b72a4cf45cd92
javierjericm <javierjericm@gmail.com>
2025-03-04 22:24:48 +0800

    Fixed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import Footer from "./components/Footer";
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,63 +1,63 @@
-import React from "react";
+// import { Link } from "react-router-dom";
 
-const Header = () => {
-  return (
-    <>
-      <header class="navbar">
-        <div class="top-bar">
-          <div class="left-links">
-            <a href="#">Seller Centre</a>
-            <a href="#">Start Selling</a>
-            <a href="#">Download</a>
-            <a href="#">
-              Follow us on <ion-icon name="logo-facebook"></ion-icon>{" "}
-              <ion-icon name="logo-instagram"></ion-icon>
-            </a>
-          </div>
-          <div class="right-links">
-            <a href="#">
-              Notifications <i class="fas fa-bell"></i>
-            </a>
-            <a href="#">
-              Help <i class="fas fa-question-circle"></i>
-            </a>
-            <a href="#">
-              English <i class="fas fa-globe"></i>
-            </a>
-            <a href="#" class="sign-up">
-              Sign Up
-            </a>
-            <a href="#" class="login">
-              Login
-            </a>
-          </div>
-        </div>
-        <div class="main-nav">
-          <div class="logo">LOGO</div>
-          <div class="search-bar">
-            <input type="text" placeholder="your first order"></input>
-            <button>
-              <ion-icon name="search-outline"></ion-icon>
-            </button>
-          </div>
-          <div class="cart-icon">
-            <ion-icon name="cart-outline"></ion-icon>
-          </div>
-        </div>
-        <div class="suggestions">
-          <a href="#">Shoes & Clothing</a>
-          <a href="#">Pet Supplies</a>
-          <a href="#">Toys & Hobbies</a>
-          <a href="#">Health & Wellness</a>
-          <a href="#">Home & Living</a>
-          <a href="#">Sex Toy</a>
-          <a href="#">Beauty & Personal Care</a>
-          <a href="#">Books & Stationery</a>
-          <a href="#">Industrial & Professional Equipment</a>
-        </div>
-      </header>
-    </>
-  );
-};
+// const Header = () => {
+//   return (
+//     <>
+//       <header className="navbar">
+//         <div className="top-bar">
+//           <div className="left-links">
+//             <a href="#">Seller Centre</a>
+//             <a href="#">Start Selling</a>
+//             <a href="#">Download</a>
+//             <a href="#">
+//               Follow us on <ion-icon name="logo-facebook"></ion-icon>{" "}
+//               <ion-icon name="logo-instagram"></ion-icon>
+//             </a>
+//           </div>
+//           <div className="right-links">
+//             <a href="#">
+//               Notifications <i className="fas fa-bell"></i>
+//             </a>
+//             <a href="#">
+//               Help <i className="fas fa-question-circle"></i>
+//             </a>
+//             <a href="#">
+//               English <i className="fas fa-globe"></i>
+//             </a>
+//             <a href="#" className="sign-up">
+//               <Link to={"/register"}>Register</Link>
+//             </a>
+//             <a href="#" className="login">
+//               <Link to={"/login"}>Login</Link>
+//             </a>
+//           </div>
+//         </div>
+//         <div className="main-nav">
+//           <div className="logo">LOGO</div>
+//           <div className="search-bar">
+//             <input type="text" placeholder="your first order"></input>
+//             <button>
+//               <ion-icon name="search-outline"></ion-icon>
+//             </button>
+//           </div>
+//           <div className="cart-icon">
+//             <ion-icon name="cart-outline"></ion-icon>
+//           </div>
+//         </div>
+//         <div className="suggestions">
+//           <a href="#">Shoes & Clothing</a>
+//           <a href="#">Pet Supplies</a>
+//           <a href="#">Toys & Hobbies</a>
+//           <a href="#">Health & Wellness</a>
+//           <a href="#">Home & Living</a>
+//           <a href="#">Sex Toy</a>
+//           <a href="#">Beauty & Personal Care</a>
+//           <a href="#">Books & Stationery</a>
+//           <a href="#">Industrial & Professional Equipment</a>
+//         </div>
+//       </header>
+//     </>
+//   );
+// };
 
-export default Header;
+// export default Header;
