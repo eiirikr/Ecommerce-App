ffb4324248087993562770f6c8bbb87cfb051410
javierjericm <javierjericm@gmail.com>
2025-03-05 23:22:35 +0800

    Added useReducer in store configuration

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: {
+      user: userReducer,
+    },
+  },
 });
