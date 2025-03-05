6f39bd0bae2fef3cddc687afa442f313d58c8623
javierjericm <javierjericm@gmail.com>
2025-03-05 22:29:56 +0800

    Added refreshToken api

--- Frontend/src/common/SummaryApi.js
+++ Frontend/src/common/SummaryApi.js
@@ -21,6 +21,10 @@ const SummaryApi = {
     url: "api/user/reset-password",
     method: "put",
   },
+  refreshToken: {
+    url: "api/user/refresh-token",
+    method: "post",
+  },
 };
 
 export default SummaryApi;
