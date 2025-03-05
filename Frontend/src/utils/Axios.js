16da2d2d019977d5d6582d0388d992a15616b7d6
javierjericm <javierjericm@gmail.com>
2025-03-05 22:33:07 +0800

    Added access token in the header

--- Frontend/src/utils/Axios.js
+++ Frontend/src/utils/Axios.js
@@ -1,9 +1,25 @@
 import axios from "axios";
-import { baseURL } from "../common/SummaryApi";
+import SummaryApi, { baseURL } from "../common/SummaryApi";
 
 const Axios = axios.create({
   baseURL: baseURL,
   withCredentials: true,
 });
+
+// Sending access token in the header
+Axios.interceptors.request.use(
+  async (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default Axios;
