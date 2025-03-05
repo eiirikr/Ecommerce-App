bc62ad5eabeac82a58c3d1ee06b29d716fe0d130
javierjericm <javierjericm@gmail.com>
2025-03-05 22:47:15 +0800

    configure redux in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./route/index";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
